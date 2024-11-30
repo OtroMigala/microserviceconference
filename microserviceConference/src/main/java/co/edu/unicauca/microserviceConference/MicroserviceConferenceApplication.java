f720c2a6fdbc34c7ecca2e2421430023013c4a8e
Juan Alejandro <juanalejocardenas@unicauca.edu.co>
2024-11-29 19:31:23 -0500

    primer commit

---
+++ microserviceConference/src/main/java/co/edu/unicauca/microserviceConference/MicroserviceConferenceApplication.java
@@ -0,0 +1,14 @@
+package co.edu.unicauca.microserviceConference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConferenceApplication.class, args);
+		System.out.print("Ejecutando MicroserviceConferenceApplication...");
+	}
+
+}
